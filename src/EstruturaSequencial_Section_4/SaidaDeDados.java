934b734945f3ec6922e2f9526d31a2d6d45e58d1
Davi <batatovisk0330@gmail.com>
2025-12-02 23:09:23 -0300

    saida de dadods java u

---
+++ src/EstruturaSequencial_Section_4/SaidaDeDados.java
@@ -0,0 +1,8 @@
+package EstruturaSequencial_Section_4;
+
+public class SaidaDeDados {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");//Quebra a linha
+        System.out.print("Olá mundo"); //Sem quebra de linha
+    }
+}
