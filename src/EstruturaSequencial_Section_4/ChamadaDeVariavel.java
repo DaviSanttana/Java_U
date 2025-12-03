a80bf49a1986fabd18cd364fdf1f1ffb9dd3dbb4
DaviSanttana <batatoviski0330@gmail.com>
2025-12-02 23:09:23 -0300

    saida de dadods java u

---
+++ src/EstruturaSequencial_Section_4/ChamadaDeVariavel.java
@@ -0,0 +1,11 @@
+package EstruturaSequencial_Section_4;
+
+public class ChamadaDeVariavel {
+    public static void main(String[] args) {
+        int x = 12;
+        double y = 155.00;
+        System.out.println(x); // chamando a variavel
+        System.out.print("A variabvel x vale "+x);
+        System.out.print("A variabvel y vale "+y);
+    }
+}
