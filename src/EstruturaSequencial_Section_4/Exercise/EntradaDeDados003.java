01af5ca8354090a2657fc1f04124d56a595a6a68
DaviSanttana <batatoviski0330@gmail.com>
2026-01-12 15:40:56 -0300

    Programming Logic - Data Entry

---
+++ src/EstruturaSequencial_Section_4/Exercise/EntradaDeDados003.java
@@ -0,0 +1,17 @@
+package EstruturaSequencial_Section_4.Exercise;
+
+import java.util.Scanner;
+
+public class EntradaDeDados003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = sc.next();
+        System.out.print("How old are you? ");
+        int age = sc.nextInt();
+
+        System.out.println("Good morning "+name+ ", you are "+age + " years old");
+
+        sc.close();
+    }
+}
---
+++ src/EstruturaSequencial_Section_4/Exercise/EntradaDeDados004.java
@@ -0,0 +1,19 @@
+package EstruturaSequencial_Section_4.Exercise;
+
+import java.util.Scanner;
+
+public class EntradaDeDados004 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Type a word: ");
+        String word = sc.next();
+
+        String palavraInvertida = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            palavraInvertida += word.charAt(i);
+        }
+        System.out.println(palavraInvertida);
+
+        sc.close();
+    }
+}
