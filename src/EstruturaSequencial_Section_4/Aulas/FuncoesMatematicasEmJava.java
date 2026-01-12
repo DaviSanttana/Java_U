7196f82c5d586a64864bd07ce9c4c3fe343e0e57
Davi <batatovisk0330@gmail.com>
2026-01-12 19:13:29 -0300

    Programming Logic - Data Entry

---
+++ src/EstruturaSequencial_Section_4/Aulas/FuncoesMatematicasEmJava.java
@@ -0,0 +1,17 @@
+package EstruturaSequencial_Section_4.Aulas;
+
+public class FuncoesMatematicasEmJava {
+    public static void main(String[] args) {
+        double x = 3.0;
+        double y = 4.5;
+        double z = -5.0;
+        double A, B, C;
+
+
+        A = Math.sqrt(x);
+        B = Math.pow(x, y );
+        C = Math.sqrt(z);
+
+
+    }
+}
