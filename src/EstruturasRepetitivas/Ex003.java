a7ff80d80a14cded38c9bc22a8f78ec5a78e0d1e
Davi <batatovisk0330@gmail.com>
2026-04-10 23:30:02 -0300

    so faz o trampo

---
+++ src/EstruturasRepetitivas/Ex003.java
@@ -0,0 +1,27 @@
+package EstruturasRepetitivas;
+
+import java.util.Scanner;
+
+public class Ex003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tipoCombustivel = sc.nextInt();// 1-Alcool | 2-Gasolina | 3-Diesel | 4-Fim
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+
+        while (tipoCombustivel != 4) {
+            if (tipoCombustivel == 1) {
+                alcool += 1;
+            } else if (tipoCombustivel == 2) {
+                gasolina += 1;
+            } else if(tipoCombustivel == 3){
+                diesel += 1;
+            }
+            tipoCombustivel = sc.nextInt();
+
+        }
+        sc.close();
+        System.out.println("Muito obrigado\nAlcool: " + alcool + "\nGasolina: " + gasolina + "\nDiesel: " + diesel);
+    }
+}
