449c1e3cb9dab35a18d68a41b5d2ac7bbf01ace2
DaviSanttana <batatoviski0330@gmail.com>
2026-04-12 21:16:13 -0300

    so faz o trampo

---
+++ src/EstruturasRepetitivas/For/Ex002.java
@@ -0,0 +1,25 @@
+package EstruturasRepetitivas.For;
+
+import java.util.Scanner;
+
+public class Ex002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dentro = 0;
+        int fora = 0;
+        System.out.print("Quantos valores quer digitar: ");
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.print("Qual o numero: ");
+            int valor = sc.nextInt();
+            if (10 <= valor && valor <= 20) {
+                dentro += 1;
+            }else {
+                fora += 1;
+            }
+        }
+        System.out.println("Numeros dentro do intervalo de 10 a 20: "+dentro);
+        System.out.println("Numeros fora do intervalo de 10 a 20: "+fora);
+    }
+    
+}
