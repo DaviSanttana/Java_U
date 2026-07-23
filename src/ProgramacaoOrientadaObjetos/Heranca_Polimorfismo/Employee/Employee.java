f96177ba3e9c5305e9a1f1ac76c326f92efd2429
DaviSanttana <batatoviski0330@gmail.com>
2026-07-23 12:56:21 -0300

    Herança e polimorfismo em Java - sobrecarga e sobrescrita

--- src/ProgramacaoOrientadaObjetos/Heranca_Polimorfismo/Employee/Employee.java
+++ src/ProgramacaoOrientadaObjetos/Heranca_Polimorfismo/Employee/Employee.java
@@ -1,11 +1,26 @@
 package ProgramacaoOrientadaObjetos.Heranca_Polimorfismo.Employee;
 
 public sealed abstract class Employee permits Salesman, Maneger {
-    private String code;
-    private String name;
-    private String adress;
-    private int age;
-    private  double salary;
+    protected String code;
+    protected String name;
+    protected String adress;
+    protected int age;
+    protected   double salary;
+
+    public Employee(String code,
+                    String name,
+                    String adress,
+                    int age,
+                    double salary) {
+        this.code = code;
+        this.name = name;
+        this.adress = adress;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public Employee() {
+    }
 
     public String getCode() {
         return code;
@@ -46,4 +61,11 @@ public sealed abstract class Employee permits Salesman, Maneger {
     public void setSalary(double salary) {
         this.salary = salary;
     }
+
+    public abstract double getFullSalary();
+
+    public double getFullSalary(double extra) {
+        return getFullSalary() + extra;
+    }
+
 }
--- src/ProgramacaoOrientadaObjetos/Heranca_Polimorfismo/Employee/Salesman.java
+++ src/ProgramacaoOrientadaObjetos/Heranca_Polimorfismo/Employee/Salesman.java
@@ -2,6 +2,32 @@ package ProgramacaoOrientadaObjetos.Heranca_Polimorfismo.Employee;
 
 public non-sealed class Salesman extends Employee{
     private  double percentPerSold;
+    private  double soldAMount;
+
+    public Salesman(String code,
+                    String name,
+                    String adress,
+                    int age,
+                    double salary,
+                    double percentPerSold,
+                    double soldAMount) {
+        super(code, name, adress, age, salary);
+        this.percentPerSold = percentPerSold;
+        this.soldAMount = soldAMount;
+    }
+
+    @Override
+    public String getCode(){
+        return "SL" + this.code;
+    }
+
+    @Override
+    public double getFullSalary() {
+        return this.salary + ((soldAMount * percentPerSold)/100);
+    }
+
+    public Salesman() {
+    }
 
     public double getPercentPerSold() {
         return percentPerSold;
@@ -11,5 +37,11 @@ public non-sealed class Salesman extends Employee{
         this.percentPerSold = percentPerSold;
     }
 
+    public double getSoldAMount() {
+        return soldAMount;
+    }
 
+    public void setSoldAMount(double soldAMount) {
+        this.soldAMount = soldAMount;
+    }
 }
