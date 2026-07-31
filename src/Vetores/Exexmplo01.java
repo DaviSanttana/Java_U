d4238f383818f5d317009a71137a48b974b3bbe5
Davi Gabriel <seu-email@exemplo.com>
2026-07-31 16:25:24 -0300

    interfaces funcionais

---
+++ src/Vetores/Exexmplo01.java
@@ -0,0 +1,27 @@
+package Vetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exexmplo01 {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Quantas alturas quer inserir: ");
+        int n = scanner.nextInt();
+        double[] vec = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            vec[i] = scanner.nextDouble();
+        }
+
+        double sum = 0;
+        for (int i = 0; i < n ; i++) {
+            sum += vec[i];
+        }
+        double avg = sum/n;
+        System.out.println("A media é: "+ avg);
+        scanner.close();
+    }
+}
+
