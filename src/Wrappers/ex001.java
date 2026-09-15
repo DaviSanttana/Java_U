766795e1d26e0923c75c21e9d0d3f2f3898a4640
Davi Santtana <batatovisk0330@gmail.com>
2026-09-15 16:13:42 -0300

    wrappers

---
+++ src/Wrappers/ex001.java
@@ -0,0 +1,14 @@
+package Wrappers;
+
+public class ex001 {
+    static void main() {
+        boolean b; // Boolean
+        byte by;  //Byte
+        short sh; //Short
+        char ch;  //Character
+        int in;  //Integer
+        float f;  //Float
+        long l; //Long
+        double d; // Double
+    }
+}
